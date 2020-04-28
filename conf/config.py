dde309da6e015be1ebd6eea7bb7c2dc1890ad924
hazuki-u <64446954+hazuki-u@users.noreply.github.com>
2020-04-28 10:11:45 +0900

    Update config.py

--- conf/config.py
+++ conf/config.py
@@ -25,15 +25,15 @@ DEFAULT_LANG = 'ja'
 TZone = "Asia/Tokyo"
 
 # account
-ADMIN_ACCOUNT = "admin@xxx.com"
-DOMAIN_ID = 0
+ADMIN_ACCOUNT = "h-uchida@kumiai-siryo"
+DOMAIN_ID = 10187082
 
 # api
-API_ID = "kr1EHxxxxfJVz"
-SERVER_CONSUMER_KEY = "To8SnC7sxxxxxjqXZhO"
+API_ID = "jp1NwJGMWFpXT"
+SERVER_CONSUMER_KEY = "tZlJLPZtCwDrr_kjtftg"
 
-SERVER_ID = "96460cc1e778402xxxxxfe35fa97ce76"
-SECRET_KEY_NAME = "private_201xxxxx164308.key"
+SERVER_ID = "6638ca3a88ef499783fd89d71bf653b4"
+SECRET_KEY_NAME = "private_20200428093800.key"
 
 # LOCAL ADDRESS
 LOCAL_ADDRESS = "https://[Heroku app name].herokuapp.com/"
